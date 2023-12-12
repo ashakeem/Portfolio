ce959d2c4f1bd522e073f05b46af1665eb3039ad
Ayomide Hakeem <125868067+ashakeem@users.noreply.github.com>
2023-12-12 12:55:06 -0800

    Update TypewriterTitle.jsx

--- src/components/TypewriterTitle.jsx
+++ src/components/TypewriterTitle.jsx
@@ -6,7 +6,7 @@ const TypewriterTitle = () => {
     <TypewriterComponent
       options={{
         loop: true,
-        delay: 50,
+        delay: 55,
       }}
       onInit={(typewriter) => {
         typewriter
