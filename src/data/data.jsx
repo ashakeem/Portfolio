9e0fb825ab8980e6063e72640bf4139c4c250473
Ayomide Hakeem <125868067+ashakeem@users.noreply.github.com>
2023-12-24 04:00:35 -0800

    Projects

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -5,11 +5,13 @@ import ProdAI from "../assets/projects/productivityai.png";
 export const data = [
   {
     id: 1,
-    name: "RyanGPT is an interactive Python app powered by OpenAI's GPT-3 model and Streamlit, allowing users to ask questions, engage in discussions, or chat about any topic.",
-    image: RyanGPT,
-    github: "https://github.com/ashakeem/RyanGPT",
-    live: "",
+    name: "Currently Developing an Artificial Inteligence(AI) Productivity Application. ",
+    image: ProdAI,
+    github: "",
+    live: "https://productivity-ai.vercel.app/",
   },
+
+  
   {
     id: 2,
     name: " CryptoHub is a dynamic React web app with Firebase integration for authentication and secure cryptocurrency storage. Experience real-time data, making it easy to view, track, and search cryptocurrencies.",
@@ -17,12 +19,13 @@ export const data = [
     github: "https://github.com/ashakeem/CryptoHub",
     live: "https://cryptohub-51b56.web.app/",
   },
-
+  
   {
     id: 3,
-    name: "Currently Developing an Artificial Inteligence(AI) Productivity Application. ",
-    image: ProdAI,
-    github: "",
-    live: "https://productivity-ai.vercel.app/",
+    name: "RyanGPT is an interactive Python app powered by OpenAI's GPT-3 model and Streamlit, allowing users to ask questions, engage in discussions, or chat about any topic.",
+    image: RyanGPT,
+    github: "https://github.com/ashakeem/RyanGPT",
+    live: "",
   },
+ 
 ];
