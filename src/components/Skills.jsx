8f57d9dfba54817ebb9f9b9cf502e11e9fff8c2f
Ayomide Hakeem <125868067+ashakeem@users.noreply.github.com>
2024-01-01 13:15:25 -0800

    updated skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -51,23 +51,25 @@ const Skills = () => {
           </div>
           <div className="shadow-md shadow-[#161b22] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={Next} alt="HTML icon" />
-            <p className="my-4">NEXT JS</p>
+            <p className="my-4">NEXT.js</p>
           </div>
-          <div className="shadow-md shadow-[#161b22] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={GitHub} alt="HTML icon" />
-            <p className="my-4">GITHUB</p>
-          </div>
+
           <div className="shadow-md shadow-[#161b22] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={Node} alt="HTML icon" />
             <p className="my-4">NODE JS</p>
           </div>
           <div className="shadow-md shadow-[#161b22] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Tailwind} alt="HTML icon" />
+            <p className="my-4">TAILWIND</p>
+          </div>
+          <div className="shadow-md shadow-[#161b22] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={Mongo} alt="HTML icon" />
             <p className="my-4">MONGO DB</p>
           </div>
+
           <div className="shadow-md shadow-[#161b22] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={Tailwind} alt="HTML icon" />
-            <p className="my-4">TAILWIND</p>
+            <img className="w-20 mx-auto" src={GitHub} alt="HTML icon" />
+            <p className="my-4">GITHUB</p>
           </div>
         </div>
       </div>
