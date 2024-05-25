e8d43d670239d7211d8d9bca5ea74d21bbc91e4d
Ayomide Hakeem <ayomidesuleimanh@gmail.com>
2024-05-24 19:03:06 -0700

    added stock market portfolio

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1,26 +1,27 @@
 import Portfolio from "../assets/projects/PortFolio.png";
 import ProdAI from "../assets/projects/productivityai.png";
-import Lexy from "../assets/projects/lexy.png";
+// import Lexy from "../assets/projects/lexy.png";
 import ExerciseCounter from "../assets/exercise-counter.png";
+import SAndP500 from "../assets/sp500.png";
 
 export const data = [
   {
     id: 1,
     name: "Productivity AI. Currently Developing an Artificial Intelligence Productivity Application. ",
     image: ProdAI,
-    github: "",
+    github: "https://github.com/ashakeem/Productivity-AI",
     live: "https://productivity-ai.vercel.app/",
     demo: "",
   },
 
-  {
-    id: 2,
-    name: "Lexy.ai, Currently Developing an Artificial Intelligence Study Tool. ",
-    image: Lexy,
-    github: "",
-    live: "",
-    demo: "",
-  },
+  // {
+  //   id: 2,
+  //   name: "Lexy.ai, Currently Developing an Artificial Intelligence Study Tool. ",
+  //   image: Lexy,
+  //   github: "",
+  //   live: "",
+  //   demo: "",
+  // },
 
   {
     id: 3,
@@ -37,5 +38,13 @@ export const data = [
     github: "https://github.com/ashakeem/AI_Exercise_Rep_Counter",
     live: "",
     demo: "https://www.loom.com/share/6201f097125148608c2802a0366d4508?sid=0dfb1c3c-2f4c-42a9-8ccd-fa2a4b298c82",
+  },
+  {
+    id: 5,
+    name: "S&P 500 Index Price Prediction ",
+    image: SAndP500,
+    github: "https://github.com/ashakeem/sp500_predictor",
+    live: "",
+    demo: "",
   },
 ];
