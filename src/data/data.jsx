9dc07fa86e4d65472bfd81f95e311118f5649c9a
Ayomide Hakeem <ayomidesuleimanh@gmail.com>
2024-06-07 13:46:48 -0400

    updated Projects with Lexy.ai

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1,12 +1,20 @@
 import Portfolio from "../assets/projects/PortFolio.png";
 import ProdAI from "../assets/projects/productivityai.png";
-// import Lexy from "../assets/projects/lexy.png";
+import Lexy from "../assets/projects/lexy.png";
 import ExerciseCounter from "../assets/exercise-counter.png";
 import SAndP500 from "../assets/sp500.png";
 
 export const data = [
   {
     id: 1,
+    name: "Lexy.ai, Currently Developing an Artificial Intelligence Study Tool. Click Live Website to see Figma Prototype or Demo to see the current progress of the project",
+    image: Lexy,
+    github: "",
+    live: "https://www.figma.com/proto/RtlQEP3bdIU9gMQFfxaYGt/Lexy.ai?node-id=2-40&t=7sp4HJyolCWSjZAR-1&scaling=scale-down&page-id=0%3A1",
+    demo: "https://www.loom.com/share/069fb82cf036457eb3dd0cd36f0d76d8",
+  },
+  {
+    id: 2,
     name: "Productivity AI. Currently Developing an Artificial Intelligence Productivity Application. ",
     image: ProdAI,
     github: "https://github.com/ashakeem/Productivity-AI",
@@ -14,23 +22,9 @@ export const data = [
     demo: "",
   },
 
-  // {
-  //   id: 2,
-  //   name: "Lexy.ai, Currently Developing an Artificial Intelligence Study Tool. ",
-  //   image: Lexy,
-  //   github: "",
-  //   live: "",
-  //   demo: "",
-  // },
+ 
 
-  {
-    id: 3,
-    name: "My Personal Portfolio Website",
-    image: Portfolio,
-    github: "",
-    live: "https://ayomidehakeem.dev",
-    demo: "",
-  },
+ 
   {
     id: 4,
     name: "AI Exercise Repetition Counter",
@@ -40,11 +34,21 @@ export const data = [
     demo: "https://www.loom.com/share/6201f097125148608c2802a0366d4508?sid=0dfb1c3c-2f4c-42a9-8ccd-fa2a4b298c82",
   },
   {
-    id: 5,
+    id: 3,
     name: "S&P 500 Index Price Prediction ",
     image: SAndP500,
     github: "https://github.com/ashakeem/sp500_predictor",
     live: "",
     demo: "",
   },
+  
+  {
+    id: 5,
+    name: "My Personal Portfolio Website",
+    image: Portfolio,
+    github: "",
+    live: "https://ayomidehakeem.dev",
+    demo: "",
+  },
+
 ];
