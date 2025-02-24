c2eed9ff6f2bffa31ea460206153f4950a53b8e6
Ayomide Hakeem <ayomidesuleimanh@gmail.com>
2025-02-23 21:46:40 -0800

    asset bug fix

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1,17 +1,8 @@
-
-  import ProdAI from "../assets/productivityai.png";
-  import ExerciseCounter from "../assets/exercise.png";
-  import MaxxedAI from "../assets/maxxedai.png";
-  import FirstYear from "../assets/firstyear.png";
-  import ContentBuddy from "../assets/contentbuddy.png";
-
 export const data = [
- 
-  
   {
     id: 4,
     name: "AI Exercise Repetition Counter",
-    image: ExerciseCounter,
+    image: "/exercise.png",
     github: "https://github.com/ashakeem/AI_Exercise_Rep_Counter",
     live: "",
     demo: "https://www.loom.com/share/6201f097125148608c2802a0366d4508?sid=0dfb1c3c-2f4c-42a9-8ccd-fa2a4b298c82",
@@ -20,7 +11,7 @@ export const data = [
   {
     id: 5,
     name: "FirstYear.ai. An Artificial Intelligence First Year Student Assistant.",
-    image: FirstYear,
+    image: "/firstyear.png",
     github: "https://github.com/ashakeem/FirstYearAI",
     live: "https://first-year.vercel.app/",
   },
@@ -28,14 +19,14 @@ export const data = [
   {
     id: 6,
     name: "ContentBuddy.ai. An Artificial Intelligence Content Creation Tool.",
-    image: ContentBuddy,
+    image: "/contentbuddy.png",
     github: "https://github.com/ashakeem/ContentBuddyAI",
     live: "https://content-buddy-ai-gew7.vercel.app/",
   },
   {
     id: 1,
     name: "Maxxed AI (Currently Developing)",
-    image: MaxxedAI,
+    image: "/maxxedai.png",
     github: "",
     live: "",
     demo: "",
@@ -43,20 +34,19 @@ export const data = [
   {
     id: 3,
     name: "Productivity AI. An Artificial Intelligence Productivity Application. ",
-    image: ProdAI,
+    image: "/productivityai.png",
     github: "https://github.com/ashakeem/Productivity-AI",
     live: "https://productivity-ai.vercel.app/",
     demo: "",
   },
 
+  // Uncomment if you want to add Lexy.ai back
   // {
   //   id: 2,
   //   name: "Lexy.ai, Currently Developing an Artificial Intelligence Study Tool. Click Live Website to see Figma Prototype or Demo to see the current progress of the project",
-  //   image: Lexy,
+  //   image: "/lexy.png",
   //   github: "",
   //   live: "https://www.figma.com/proto/RtlQEP3bdIU9gMQFfxaYGt/Lexy.ai?node-id=2-40&t=7sp4HJyolCWSjZAR-1&scaling=scale-down&page-id=0%3A1",
   //   demo: "https://www.loom.com/share/069fb82cf036457eb3dd0cd36f0d76d8",
   // },
- 
-
 ];
